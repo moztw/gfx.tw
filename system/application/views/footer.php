3f88ceb6b5e2351fe8d6bf551f9fe4b50f30a4f8
Timothy Guan-tin Chien <timdream@gmail.com>
2009-01-13 11:53:54 +0000

    footer text

--- system/application/views/footer.php
+++ system/application/views/footer.php
@@ -1,3 +1,7 @@
+	<div id="footer">
+		<p>&copy; <a href="<?php print site_url('about'); ?>">Contributors to project gfx</a>. <a href="http://www.moztw.org/"><img src="http://www.moztw.org/images/moztw_80x15.png" alt="MozTW" /></a> MozTW，台灣 Mozilla 社群。</p>
+		<p>Not affiliated with <a href="http://www.mozilla.org/">Mozilla</a>. Firefox and the Firefox logos are trademarks of the Mozilla Fundation.</p>
+	</div>
 	<!-- <?php print 'Elapsed Time: ' . $this->benchmark->elapsed_time() . ', Memory usage: ' . $this->benchmark->memory_usage() . ', from DB: ' . $db; ?> -->
 	</body>
 </html>
