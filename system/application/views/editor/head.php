3816bb3ea30ba3de785d9d2e435bd3588eb80e33
Timothy Guan-tin Chien <timdream@gmail.com>
2009-01-13 04:01:51 +0000

    Move Editor views and clean up

---
+++ system/application/views/editor/head.php
@@ -0,0 +1,18 @@
+<?php
+print '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+
+<!DOCTYPE html 
+     PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-TW" lang="zh-TW">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<title>編輯您的抓火狐推薦頁！</title>
+	<link rel="stylesheet" type="text/css" href="<?php print base_url() ?>style.css" />
+	<script type="text/javascript" src="<?php print site_url('js/mootools-1.2.1-core.js'); ?>"></script>
+	<script type="text/javascript" src="<?php print site_url('js/global.js'); ?>" charset="UTF-8"></script>
+	<script type="text/javascript" src="./js/editor.js" charset="UTF-8"></script>
+	<script type="text/javascript" src="./swfupload/swfupload-min.js" charset="UTF-8"></script>
+	<script type="text/javascript" src="./js/mootools-1.2-more.js"></script>
+</head>
