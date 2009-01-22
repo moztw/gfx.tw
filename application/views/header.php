01221274ac5f68a92d2e68d4d5f120a3df80bef2
Timothy Guan-tin Chien <timdream@gmail.com>
2009-01-22 17:50:07 +0000

    Login dialog renew

--- application/views/header.php
+++ application/views/header.php
@@ -18,10 +18,20 @@ if (isset($id)) {
 		<form action="<?php print site_url('auth/login'); ?>" method="post">
 			<p><label for="openid-identifier">您的 OpenID 網址: </label><input type="text" name="openid-identifier" id="openid-identifier" value="" /> <input type="submit" value="登入" /></p>
 			<h3>OpenID 是什麼？</h3>
-			<p>OpenID 其他網站的帳號登入敝站，避免再次記憶帳號與輸入個人資訊的困擾。若您曾在下列網站註冊過，您可以選擇想要使用的 OpenID：</p>
+			<p>OpenID 讓您使用其他網站的帳號登入敝站，避免再次記憶帳號與輸入資料的困擾。若您曾在下列網站註冊過，您可以選擇想要使用的 OpenID：</p>
 			<p><label for="openid_sp">OpenID 服務商: </label><select id="openid_sp">
-				<option value="" selected="selected">(選擇服務商)</option>
-				<option value="http://[帳號].wordpress.com/">WordPress.com</option>
+				<option value="" label="(選擇服務商)" selected="selected">(選擇服務商)</option>
+				<optgroup label="不需修改網址可直接登入">
+					<option value="https://www.google.com/accounts/o8/id">Google</option>
+					<option value="https://me.yahoo.com">Yahoo!</option>
+				</optgroup>
+				<optgroup label="需在網址加入帳號">
+					<option value="openid.aol.com/[帳號]">AIM</option>
+					<option value="[帳號].livejournal.com">LiveJournal</option>
+					<option value="[帳號].myid.tw">myID.tw</option>
+					<option value="profile.typekey.com/[帳號]">TypePad</option>
+					<option value="[帳號].wordpress.com">WordPress.com</option>
+				</optgroup>
 			</select></p>
 			<p>若您真的沒有任何 OpenID，或是不願意讓敝站帳號與之連結，您可以到 <a href="http://myid.tw/" class="newwindow">myID.tw</a> 申請一個屬於您的 OpenID。</p>
 		</form>
