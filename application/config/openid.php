14d4be6271d251d0b1160807ec65c46a8ce6bc58
Timothy Guan-tin Chien <timdream@gmail.com>
2009-01-23 00:56:15 +0000

    Change openid config

--- application/config/openid.php
+++ application/config/openid.php
@@ -1,7 +1,7 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 $config['openid_storepath'] = '/tmp';
-$config['openid_policy'] = 'auth/policy';
+$config['openid_policy'] = 'about/legal';
 $config['openid_required'] = array('nickname');
 $config['openid_optional'] = array('fullname', 'email');
 $config['openid_request_to'] = 'auth/check';
