6e14ebaba6a235ace4d204e638b85d77ff74315e
Timothy Guan-tin Chien <timdream@gmail.com>
2009-02-27 23:25:23 +0000

    make text.js localizable

--- application/views/english/page_template.php
+++ application/views/english/page_template.php
@@ -6,12 +6,12 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-	<link rel="stylesheet" type="text/css" href="http://stage.gfx.tw/style.css" />
-	<link rel="stylesheet" type="text/css" href="http://stage.gfx.tw/style/ui.all.css" />
-	<script type="text/javascript" src="http://stage.gfx.tw/js/jquery-1.3.1.min.js"></script>
-	<script type="text/javascript" src="http://stage.gfx.tw/js/jquery-ui-personalized-1.6.min.js"></script>
-	<script type="text/javascript" src="http://stage.gfx.tw/js/text.js" charset="UTF-8"></script>
-	<script type="text/javascript" src="http://stage.gfx.tw/js/global.js" charset="UTF-8"></script>
+	<link rel="stylesheet" type="text/css" href="<?php print site_url('style.css') ?>" />
+	<link rel="stylesheet" type="text/css" href="<?php print site_url('style/ui.all.css') ?>" />
+	<script type="text/javascript" src="<?php print site_url('js/jquery-1.3.1.min.js') ?>"></script>
+	<script type="text/javascript" src="<?php print site_url('js/jquery-ui-personalized-1.6.min.js') ?>"></script>
+	<script type="text/javascript" src="<?php print site_url('js/' . $this->config->item('language') . '-text.js') ?>" charset="UTF-8"></script>
+	<script type="text/javascript" src="<?php print site_url('js/global.js') ?>" charset="UTF-8"></script>
 {meta}
 </head>
 <body>
@@ -26,7 +26,7 @@
 {/messages}
 {content}
 	<div id="footer">
-		<p>Copyright &copy; gfx.tw | <a href="http://stage.gfx.tw/about">About Us</a> | <a href="http://stage.gfx.tw/about/legal">Legal</a> | <a href="http://www.moztw.org/">MozTW, Mozilla Taiwan Community</a> <img src="http://www.moztw.org/images/moztw_80x15.png" alt="MozTW" /></p>
+		<p>Copyright &copy; gfx.tw | <a href="<?php print site_url('about') ?>">About Us</a> | <a href="<?php print site_url('about/legal') ?>">Legal</a> | <a href="http://www.moztw.org/">MozTW, Mozilla Taiwan Community</a> <img src="http://www.moztw.org/images/moztw_80x15.png" alt="MozTW" /></p>
 		<p>Not affiliated with Mozilla. Firefox and the Firefox logos are trademarks of the <a href="http://www.mozilla.org/">Mozilla Fundation</a>.</p>
 	</div>
 	<!-- <?php print 'Elapsed Time: ' . $this->benchmark->elapsed_time() . ', Memory usage: ' . $this->benchmark->memory_usage(); ?>, from DB: {db} -->
