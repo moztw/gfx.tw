bdf10b9eea58a42bc878b86e61e7f5df972c743e
Irvin <irvinfly@gmail.com>
2009-09-08 11:29:04 +0000

    share button style test

--- application/views/traditional_chinese/user/content.php
+++ application/views/traditional_chinese/user/content.php
@@ -51,7 +51,7 @@ foreach ($features as $feature) {
 			<li><a class="newwindow" title="分享到 Facebook" href="http://www.facebook.com/sharer.php?u=<?php
 /* Facebook fetches sticker image and description from <head> */
 print urlencode(site_url($name));
-?>"><span class="sprite facebook"></span>臉書</a></li>
+?>"><span class="sprite facebook"></span></a></li>
 			<li><a class="newwindow" title="噗到 Plurk" href="http://plurk.com/?status=<?php 
 print urlencode('推薦您和' . htmlspecialchars($title) . '一起抓火狐，使用 Firefox 逛網頁！ ' 
 	. site_url(
@@ -60,14 +60,14 @@ print urlencode('推薦您和' . htmlspecialchars($title) . '一起抓火狐，�
 	. '/featurecard.png '
 	. site_url($name)
 );
-?>"><span class="sprite plurk"></span>噗浪</a></li>
+?>"><span class="sprite plurk"></span></a></li>
 			<li><a class="newwindow" title="推到 Twitter" href="http://twitter.com/home/?status=<?php
 print urlencode('推薦您和' . htmlspecialchars($title) . '一起抓火狐，使用 Firefox 逛網頁！ ' . site_url($name));
-?>"><span class="sprite twitter"></span>推特</a></li>
+?>"><span class="sprite twitter"></span></a></li>
 			<li><a class="newwindow" title="推薦到 Funp" href="http://funp.com/push/submit/?via=tools&amp;url=<?
 /* TBD: push sticker image and description to funp */
 print urlencode(site_url($name));
-?>"><span class="sprite funp"></span>推推王</a></li>
+?>"><span class="sprite funp"></span></a></li>
 		</ul>
 	</div>
 	<div id="middleblock">
