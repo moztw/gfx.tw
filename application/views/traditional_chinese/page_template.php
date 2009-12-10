db0a91d7a0a3b4b83d359efbd9d30417a0ad4ae3
Timothy Guan-tin Chien <timdream@gmail.com>
2009-12-10 04:35:59 +0000

    Change moztw.org URL, remove "www" (merge 407:408 to production)

--- application/views/traditional_chinese/page_template.php
+++ application/views/traditional_chinese/page_template.php
@@ -25,7 +25,7 @@ print '<?xml version="1.0" encoding="UTF-8"?>';
 {/messages}
 {content}
 	<div id="footer">
-		<p>版權所有 &copy; gfx.tw | <strong><a href="http://forum.moztw.org/viewforum.php?f=59">回饋意見</a></strong> | <a href="<?php print site_url('/about') ?>">關於我們</a> | <a href="<?php print site_url('/about/legal') ?>">使用條款</a> | <a href="<?php print site_url('/about/faq') ?>">常見問題</a> | <a href="http://www.moztw.org/" id="link-moztw">MozTW，台灣 Mozilla 社群 <img src="http://www.moztw.org/images/moztw_80x15.png" alt="MozTW" /></a></p>
+		<p>版權所有 &copy; gfx.tw | <strong><a href="http://forum.moztw.org/viewforum.php?f=59">回饋意見</a></strong> | <a href="<?php print site_url('/about') ?>">關於我們</a> | <a href="<?php print site_url('/about/legal') ?>">使用條款</a> | <a href="<?php print site_url('/about/faq') ?>">常見問題</a> | <a href="http://moztw.org/" id="link-moztw">MozTW，台灣 Mozilla 社群 <img src="http://moztw.org/images/moztw_80x15.png" alt="MozTW" /></a></p>
 		<p>Not affiliated with Mozilla. Firefox and the Firefox logos are trademarks of the <a href="http://www.mozilla.org/" id="link-mozilla">Mozilla Fundation</a>.</p>
 	</div>
 	<script type="text/javascript" src="<?php print site_url('js/jquery-1.3.2.min.js' . $this->config->item('gfx_suffix')) ?>"></script>
