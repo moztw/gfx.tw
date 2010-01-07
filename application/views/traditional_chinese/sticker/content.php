48d42c623e85935fd984221b33a9f3d2fae486b2
Timothy Guan-tin Chien <timdream@gmail.com>
2010-01-07 06:06:33 +0000

    Correct stickers code (merge 414:415 to production)

--- application/views/traditional_chinese/sticker/content.php
+++ application/views/traditional_chinese/sticker/content.php
@@ -14,7 +14,7 @@ $dir = site_url('/userstickers/' . dechex(intval($this->session->userdata('id'))
 				<h2>圖片宣傳小卡</h2>
 				<p><a href="<?php print site_url($name); ?>" title="連到我的抓火狐推薦頁！"><img style="margin: 5px auto; width: 200px; height: 250px" src="<?php print $dir ?>/featurecard.png?<?php print strtotime($modified) ?>" alt="連到我的抓火狐推薦頁！" /></a></p>
 				<p>HTML:</p>
-				<textarea readonly="readonly">&lt;a href="<?php print site_url($name); ?>" title="連到我的抓火狐推薦頁！"&gt;&lt;img style="margin: 5px auto; width: 200px; height: 250px" src="<?php print $dir; ?>/featurecard.png" alt="連到我的抓火狐推薦頁！"/&gt;&lt;/a&gt;</textarea>
+				<textarea readonly="readonly">&lt;a href="<?php print site_url($name); ?>" title="連到我的抓火狐推薦頁！"&gt;&lt;img style="margin: 5px auto; border: none; width: 200px; height: 250px" src="<?php print $dir; ?>/featurecard.png" alt="連到我的抓火狐推薦頁！"/&gt;&lt;/a&gt;</textarea>
 				<p>BBCode:</p>
 				<textarea readonly="readonly">[url=<?php print site_url($name); ?>][img]<?php print $dir ?>/featurecard.png[/img][/url]</textarea>
 			</div>
@@ -22,20 +22,20 @@ $dir = site_url('/userstickers/' . dechex(intval($this->session->userdata('id'))
 				<h2>小貼紙</h2>
 				<p><a href="<?php print site_url($name); ?>" title="連到我的抓火狐推薦頁！"><img style="margin: 5px auto; width: 200px; height: 100px" src="<?php print $dir ?>/smallsticker.png" alt="連到我的抓火狐推薦頁！" /></a></p>
 				<p>HTML:</p>
-				<textarea readonly="readonly">&lt;a href="<?php print site_url($name); ?>" title="連到我的抓火狐推薦頁！"&gt;&lt;img src="<?php print $dir ?>/smallsticker.png" alt="連到我的抓火狐推薦頁！"/&gt;&lt;/a&gt;</textarea>
+				<textarea readonly="readonly">&lt;a href="<?php print site_url($name); ?>" title="連到我的抓火狐推薦頁！"&gt;&lt;img style="border: none; height: 100px; width: 200px" src="<?php print $dir ?>/smallsticker.png" alt="連到我的抓火狐推薦頁！"/&gt;&lt;/a&gt;</textarea>
 				<p>BBCode:</p>
 				<textarea readonly="readonly">[url=<?php print site_url($name); ?>][img]<?php print $dir ?>/smallsticker.png[/img][/url]</textarea>
 				<p><a href="<?php print site_url($name); ?>" title="連到我的抓火狐推薦頁！"><img style="margin: 5px auto; width: 165px; height: 90px" src="<?php print $dir ?>/smallsticker2.png" alt="連到我的抓火狐推薦頁！" /></a></p>
 				<p>HTML:</p>
-				<textarea readonly="readonly">&lt;a href="<?php print site_url($name); ?>" title="連到我的抓火狐推薦頁！"&gt;&lt;img src="<?php print $dir ?>/smallsticker2.png" alt="連到我的抓火狐推薦頁！"/&gt;&lt;/a&gt;</textarea>
+				<textarea readonly="readonly">&lt;a href="<?php print site_url($name); ?>" title="連到我的抓火狐推薦頁！"&gt;&lt;img style="border: none; height: 90px; width: 165px" src="<?php print $dir ?>/smallsticker2.png" alt="連到我的抓火狐推薦頁！"/&gt;&lt;/a&gt;</textarea>
 				<p>BBCode:</p>
 				<textarea readonly="readonly">[url=<?php print site_url($name); ?>][img]<?php print $dir ?>/smallsticker2.png[/img][/url]</textarea>
 			</div>
 			<div class="desc">
 				<h2>橫向貼紙</h2>
-				<p><a href="<?php print site_url($name); ?>" title="連到我的抓火狐推薦頁！"><img style="margin: 5px auto; width: auto; height: auto" src="<?php print $dir ?>/featurecard-h.png?<?php print strtotime($modified) ?>" alt="連到我的抓火狐推薦頁！" /></a></p>
+				<p><a href="<?php print site_url($name); ?>" title="連到我的抓火狐推薦頁！"><img src="<?php print $dir ?>/featurecard-h.png?<?php print strtotime($modified) ?>" alt="連到我的抓火狐推薦頁！" /></a></p>
 				<p>HTML:</p>
-				<textarea readonly="readonly">&lt;a href="<?php print site_url($name); ?>" title="連到我的抓火狐推薦頁！"&gt;&lt;img style="margin: 5px auto; width: 200px; height: 250px" src="<?php print $dir; ?>/featurecard-h.png" alt="連到我的抓火狐推薦頁！"/&gt;&lt;/a&gt;</textarea>
+				<textarea readonly="readonly">&lt;a href="<?php print site_url($name); ?>" title="連到我的抓火狐推薦頁！"&gt;&lt;img style="border: none; height: 60px; width: 468px" src="<?php print $dir; ?>/featurecard-h.png" alt="連到我的抓火狐推薦頁！"/&gt;&lt;/a&gt;</textarea>
 				<p>BBCode:</p>
 				<textarea readonly="readonly">[url=<?php print site_url($name); ?>][img]<?php print $dir ?>/featurecard-h.png[/img][/url]</textarea>
 			</div>
