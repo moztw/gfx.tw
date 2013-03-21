7165133b673d13f2be3d4056b13e9f8a050db2fb
Timothy Guan-tin Chien <timdream@gmail.com>
2013-03-21 17:14:29 +0800

    update gfx-sample.php to better reflect the current setting

--- application/config/gfx-sample.php
+++ application/config/gfx-sample.php
@@ -1,7 +1,7 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /* Google Analytics, empty string to disable */
-$config['gfx_gaq'] = "XXXXX";
+$config['gfx_gaq'] = "";
 
 /* Change this config item to empty array if you don't need site wide message */
 $config['gfx_site_wide_message'] = array(
@@ -12,29 +12,32 @@ $config['gfx_site_wide_message'] = array(
 	)
 );
 
-/* suffix of css/js files */
-$config['gfx_suffix'] = '?0529';
+/* suffix of css/js files, used to force reload assets on the production site */
+$config['gfx_suffix'] = '';
 
 /* If given OpenID does not exist in the database,
 redirect user to a about/closetest page instead of create entry for her/him */
 $config['gfx_require_pre_authorization'] = false;
-/* This token is used to generate the md5 hash to be send back by POST requests so we can check it's origin.
+
+/* This CSRF token is used to generate the md5 hash to be send back by POST requests so we can check it's origin.
 Change this on a live site will disable all forms; user would have to reload to get the new hash */
 $config['gfx_token'] = '--secret-token-good-day-fx';
+
 /* Cache time (in seconds) */
 $config['gfx_cache_time'] = 60;
+
 /* Remember to change gfxcode.php on your phpBB installation.
 Change this token does not affect forum id and username that already in the database */
 $config['gfx_forum_auth_token'] = '--secret-md5-string hash blah kkk';
 
 /* path to font for sticker generations */
-$config['gfx_sticker_font'] = '/usr/share/fonts/truetype/ttf-custom/LiHei Pro.ttf';
+$config['gfx_sticker_font'] = '/home/moztw/gfx/var/fonts/wqy-microhei.ttc';
 
 /* This is the download url that download controller redirects users to.
 Usually this goes to Mozilla load balancing bouncer (i.e. download.mozilla.org)
-"os" variable will append to the end
-Thanks to bug 398366,do remember to change the version every time new version comes out */
-$config['gfx_downloadurl'] = 'http://download.mozilla.org/?product=firefox-3.6.3&lang=zh-TW&os=';
+"os" variable will append to the end */
+$config['gfx_downloadurl'] = 'http://download.mozilla.org/?product=firefox-latest&lang=zh-TW&os=';
+
 /* Where user will be redirect to if Javascript and User-agent both failed to detect os */
 /* TBD: instead redirect to another webpage,show up a dialog and ask for the os from user? */
 $config['gfx_downloadfallback'] = 'http://moztw.org/firefox/';
@@ -49,8 +52,9 @@ $config['gfx_amo_xpi_url'] = 'https://addons.mozilla.org/zh-TW/firefox/downloads
 
 /* Only re-fetch add-ons after the data is that old (seconds) */
 $config['gfx_amo_fetch_older_than_time'] = 7*24*60*60;
+
 /* Or it's older than a specific date stated here */
-$config['gfx_amo_fetch_older_than_date'] = strtotime('2009-03-07 10:00:00');
+$config['gfx_amo_fetch_older_than_date'] = strtotime('2010-09-10 21:10:00');
 
 /* The from: field for all the mails send from the server to the users */
 $config['gfx_mail_from_add'] = 'bot-no-reply@gfx.tw';
@@ -109,4 +113,4 @@ $config['gfx_badname'] = array(
 );
 
 /* End of file gfx.php */
-/* Location: .applications/config/gfx.php */ 
+/* Location: .applications/config/gfx.php */
